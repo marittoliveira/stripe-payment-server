3be9a3ea58abdb9f85e4ecbb9d80ff3da4e56ce2
Rafael Matos <rafaelmatostj@gmail.com>
2020-08-14 08:49:43 -0300

    vr-split

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const app = require("express")();
-var mercadopago = require("mercadopago");
+const app = require('express')();
+var mercadopago = require('mercadopago');
 var port = process.env.PORT || 9000;
 
 mercadopago.configure({
   access_token:
-    "TEST-1456055079143308-051916-8ad472fa5fd87ef418bdc7c48d9614f0-233894286",
+    'TEST-1456055079143308-051916-8ad472fa5fd87ef418bdc7c48d9614f0-233894286',
 });
 
 // app.use(require("body-parser").text());
-app.use(require("body-parser").json());
+app.use(require('body-parser').json());
 
-app.get("/", async (req, res) => {
-  res.send("welcome");
+app.get('/', async (req, res) => {
+  res.send('welcome');
 });
 
-app.post("/api/get_preference", async (req, res) => {
+app.post('/api/get_preference', async (req, res) => {
   var obj = req.body;
   // console.log(obj);
 
@@ -24,7 +24,7 @@ app.post("/api/get_preference", async (req, res) => {
     var item = {
       title: obj.title,
       quantity: 1,
-      currency_id: "BRL",
+      currency_id: 'BRL',
       unit_price: obj.service_value,
     };
 
@@ -34,8 +34,39 @@ app.post("/api/get_preference", async (req, res) => {
       date_created: new Date().toISOString(),
     };
 
+    var payments = {
+      payment_method_id: obj.method,
+      payment_type_id: obj.card,
+      token: response.id,
+      transaction_amount: obj.service_value,
+      installments: obj.plots,
+      processing_mode: 'aggregator',
+    };
+
+    var disbursements = {
+      //Values ​​rates
+      amount: obj.service_rate,
+      external_reference: obj.market,
+      collector_id: id.place,
+      application_fee: 20,
+      money_release_days: 30,
+      //seller values
+      amount: obj.service_withfee,
+      external_reference: obj.seller,
+      collector_id: id.seller,
+      application_fee: 80,
+      money_release_days: 30,
+    };
+
+    var external_reference = {
+      id: obj.ref_transaction,
+    };
+
     preference.items = [item];
     preference.payer = payer;
+    preference.payments = payments;
+    preference.disbursements = disbursements;
+    preference.external_reference = external_reference;
 
     mercadopago.preferences
       .create(preference)
@@ -54,4 +85,4 @@ app.post("/api/get_preference", async (req, res) => {
   }
 });
 
-app.listen(port, () => console.log("Listening " + port));
+app.listen(port, () => console.log('Listening ' + port));
