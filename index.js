5978e74e41054c8d17c861c9367cc6fadf4032b2
phantalab <murtuza.labib@gmail.com>
2020-09-01 17:46:22 +0530

    tushar

--- index.js
+++ index.js
@@ -4,20 +4,19 @@
 
 // mercadopago.configure({
 //   access_token:
-//     "TEST-1456055079143308-051916-8ad472fa5fd87ef418bdc7c48d9614f0-233894286",
+//     "TEST-1456055079143308-082916-93e5ea074bc0bfd91d14ca3ad17848c5-233894286",
 // });
 
 // // app.use(require("body-parser").text());
 // app.use(require("body-parser").json());
 
 // app.get("/", async (req, res) => {
-//   res.send("welcome ml");
+//   res.send("welcome");
 // });
 
 // app.post("/api/get_preference", async (req, res) => {
 //   var obj = req.body;
-
-//   // console.log(obj);
+//   console.log(obj);
 
 //   try {
 //     var preference = {};
@@ -35,49 +34,24 @@
 //       date_created: new Date().toISOString(),
 //     };
 
-//     var payments = {
-//       payment_method_id: obj.method,
-//       payment_type_id: obj.card,
-//       token: response.id,
-//       transaction_amount: obj.service_value,
-//       installments: obj.plots,
-//       processing_mode: "aggregator",
-//     };
-
-//     var disbursements = {
-//       //Values ​​rates
-//       amount: obj.service_rate,
-//       external_reference: obj.market,
-//       collector_id: id.place,
-//       application_fee: 20,
-//       money_release_days: 30,
-//       //seller values
-//       amount: obj.service_withfee,
-//       external_reference: obj.seller,
-//       collector_id: id.seller,
-//       application_fee: 80,
-//       money_release_days: 30,
-//     };
-
-//     var external_reference = {
-//       id: obj.ref_transaction,
+//     var payment_methods = {
+//       excluded_payment_types: [{ id: "ticket" }, { id: "atm" }],
 //     };
 
 //     preference.items = [item];
 //     preference.payer = payer;
-//     preference.payments = payments;
-//     preference.disbursements = disbursements;
-//     preference.external_reference = external_reference;
+//     preference.payment_methods = payment_methods;
 
 //     mercadopago.preferences
 //       .create(preference)
 //       .then((data) => {
-//         // console.log(data);
+//         console.log(data);
 //         let r = { status: 200, id: data.body.id };
 //         res.json(r);
 //       })
 //       .catch((error) => {
 //         let r = { status: 500 };
+//         console.log(error);
 //         res.json({ r });
 //       });
 //   } catch (err) {
@@ -90,14 +64,21 @@
 
 const app = require("express")();
 var mercadopago = require("mercadopago");
+
 var port = process.env.PORT || 9000;
 
 mercadopago.configure({
   access_token:
-    "TEST-1456055079143308-082916-93e5ea074bc0bfd91d14ca3ad17848c5-233894286",
+    "TEST-5642876213767884-061008-27aa57358417722c30553630ff3fe76e-6430927",
 });
-
-// app.use(require("body-parser").text());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(require("body-parser").json());
 
 app.get("/", async (req, res) => {
@@ -106,7 +87,7 @@ app.get("/", async (req, res) => {
 
 app.post("/api/get_preference", async (req, res) => {
   var obj = req.body;
-  console.log(obj);
+  // console.log(obj);
 
   try {
     var preference = {};
@@ -124,30 +105,55 @@ app.post("/api/get_preference", async (req, res) => {
       date_created: new Date().toISOString(),
     };
 
-    var payment_methods = {
-      excluded_payment_types: [{ id: "ticket" }, { id: "atm" }],
-    };
-
     preference.items = [item];
     preference.payer = payer;
-    preference.payment_methods = payment_methods;
 
     mercadopago.preferences
       .create(preference)
       .then((data) => {
-        console.log(data);
+        // console.log(data);
         let r = { status: 200, id: data.body.id };
         res.json(r);
       })
       .catch((error) => {
         let r = { status: 500 };
-        console.log(error);
         res.json({ r });
       });
   } catch (err) {
     let r = { status: 500 };
     res.status(500).end();
   }
+});
+
+app.post("/api/process_payment", (req, res) => {
+  var payment_data = {
+    transaction_amount: parseInt(req.body.transactionAmount),
+    token: req.body.token,
+    description: req.body.description,
+    installments: parseInt(req.body.installments),
+    payment_method_id: req.body.paymentMethodId,
+    issuer_id: undefined,
+    payer: {
+      email: req.body.email,
+      identification: {
+        type: req.body.docType,
+        number: req.body.docNumber,
+      },
+    },
+  };
+  mercadopago.payment
+    .save(payment_data)
+    .then(function (response) {
+      console.log(response);
+      res.status(response.status).json({
+        status: response.body.status,
+        status_detail: response.body.status_detail,
+        id: response.body.id,
+      });
+    })
+    .catch((error) => {
+      console.log("error Found : ", error);
+    });
 });
 
 app.listen(port, () => console.log("Listening " + port));
