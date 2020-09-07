6c2cccc0fa3d4f1425b3318415d2e66f7979eef1
Uddipan <cto@meliodus.org>
2020-09-08 00:20:45 +0530

    Merge changes from uddipan branch

--- index.js
+++ index.js
@@ -1,3 +1,63 @@
+/*const app = require("express")();
+var mercadopago = require("mercadopago");
+var port = process.env.PORT || 9000;
+
+mercadopago.configure({
+  access_token:
+    "TEST-1456055079143308-051916-8ad472fa5fd87ef418bdc7c48d9614f0-233894286",
+});
+
+// app.use(require("body-parser").text());
+app.use(require("body-parser").json());
+
+app.get("/", async (req, res) => {
+  res.send("welcome");
+});
+
+app.post("/api/get_preference", async (req, res) => {
+  var obj = req.body;
+  // console.log(obj);
+
+  try {
+    var preference = {};
+
+    var item = {
+      title: obj.title,
+      quantity: 1,
+      currency_id: "BRL",
+      unit_price: obj.service_value,
+    };
+
+    var payer = {
+      email: obj.email,
+      name: obj.name,
+      date_created: new Date().toISOString(),
+    };
+
+    var payment_methods = {
+      excluded_payment_types: [{ id: "ticket" }, { id: "atm" }],
+    };
+
+    preference.items = [item];
+    preference.payer = payer;
+    preference.payment_methods = payment_methods;
+
+    mercadopago.preferences
+      .create(preference)
+      .then((data) => {
+        // console.log(data);
+        let r = { status: 200, id: data.body.id };
+        res.json(r);
+      })
+      .catch((error) => {
+        let r = { status: 500 };
+        res.json({ r });
+      });
+  } catch (err) {
+    let r = { status: 500 };
+    res.status(500).end();
+  }
+});*/
 const app = require("express")();
 var mercadopago = require("mercadopago");
 
@@ -61,6 +121,7 @@ app.post("/api/get_preference", async (req, res) => {
   }
 });
 
+
 app.post("/api/process_payment", (req, res) => {
   var payment_data = {
     transaction_amount: parseInt(req.body.transactionAmount),
@@ -70,26 +131,25 @@ app.post("/api/process_payment", (req, res) => {
     payment_method_id: req.body.paymentMethodId,
     issuer_id: undefined,
     payer: {
-      email: req.body.email,
-    },
+      email: req.body.email
+    }
   };
-  try {
-    mercadopago.payment
-      .save(payment_data)
-      .then(function (response) {
-        res.status(response.status).json({
-          status: response.body.status,
-          status_detail: response.body.status_detail,
-          id: response.body.id,
-        });
-      })
-      .catch((error) => {
-        console.log("error Found : ", error);
+  try{
+  mercadopago.payment.save(payment_data).then(function(response) {
+      res.status(response.status).json({
+        status: response.body.status,
+        status_detail: response.body.status_detail,
+        id: response.body.id
       });
-  } catch (err) {
-    console.log(err);
-    res.status(400).json(err);
+    }).catch((error) => {
+      console.log("error Found : ",error)
+    });
+  }catch(err){
+    console.log(err)
+    res.status(400).json(err)
   }
 });
+
+app.listen(port, () => console.log("Listening " + port));
 
 app.listen(port, () => console.log("Listening " + port));
