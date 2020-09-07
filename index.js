1a0835068d7dde1ade1d72136d17f86c05fe806c
Uddipan <cto@meliodus.org>
2020-09-08 01:21:42 +0530

    Updated

--- index.js
+++ index.js
@@ -1,63 +1,3 @@
-/*const app = require("express")();
-var mercadopago = require("mercadopago");
-var port = process.env.PORT || 9000;
-
-mercadopago.configure({
-  access_token:
-    "TEST-1456055079143308-051916-8ad472fa5fd87ef418bdc7c48d9614f0-233894286",
-});
-
-// app.use(require("body-parser").text());
-app.use(require("body-parser").json());
-
-app.get("/", async (req, res) => {
-  res.send("welcome");
-});
-
-app.post("/api/get_preference", async (req, res) => {
-  var obj = req.body;
-  // console.log(obj);
-
-  try {
-    var preference = {};
-
-    var item = {
-      title: obj.title,
-      quantity: 1,
-      currency_id: "BRL",
-      unit_price: obj.service_value,
-    };
-
-    var payer = {
-      email: obj.email,
-      name: obj.name,
-      date_created: new Date().toISOString(),
-    };
-
-    var payment_methods = {
-      excluded_payment_types: [{ id: "ticket" }, { id: "atm" }],
-    };
-
-    preference.items = [item];
-    preference.payer = payer;
-    preference.payment_methods = payment_methods;
-
-    mercadopago.preferences
-      .create(preference)
-      .then((data) => {
-        // console.log(data);
-        let r = { status: 200, id: data.body.id };
-        res.json(r);
-      })
-      .catch((error) => {
-        let r = { status: 500 };
-        res.json({ r });
-      });
-  } catch (err) {
-    let r = { status: 500 };
-    res.status(500).end();
-  }
-});*/
 const app = require("express")();
 var mercadopago = require("mercadopago");
 
@@ -65,7 +5,7 @@ var port = process.env.PORT || 9000;
 
 mercadopago.configure({
   access_token:
-    "TEST-1456055079143308-051916-8ad472fa5fd87ef418bdc7c48d9614f0-233894286",
+    "TEST-5642876213767884-061008-27aa57358417722c30553630ff3fe76e-6430927",
 });
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
