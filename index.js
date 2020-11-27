6b41f74d39cae48395e9a7eee329c0bd48fe03fe
Reshail Muzammil <reshailawan@gmail.com>
2020-11-27 18:20:19 +0500

    Stripe Added

--- index.js
+++ index.js
@@ -81,6 +81,30 @@ app.get("/", async (req, res) => {
   res.send("welcome");
 });
 
+app.post('/api/sub', async (req, res) => {
+  const {email, payment_method, price} = req.body;
+
+  const customer = await stripe.customers.create({
+    payment_method: payment_method,
+    email: email,
+    invoice_settings: {
+      default_payment_method: payment_method,
+    },
+  });
+
+  const subscription = await stripe.subscriptions.create({
+    customer: customer.id,
+    
+    items: [{ price }],
+    expand: ['latest_invoice.payment_intent']
+  });
+  
+  const status = subscription['latest_invoice']['payment_intent']['status'] 
+  const client_secret = subscription['latest_invoice']['payment_intent']['client_secret']
+
+  res.json({'client_secret': client_secret, 'status': status});
+})
+
 app.post("/api/get_preference", async (req, res) => {
   var obj = req.body;
   // console.log(obj);
